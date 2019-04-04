4415b39f283dde453367983209c293daacc1e3a3
Eric Ford <ebf11@psu.edu>
2019-04-04 09:59:08 -0400

    added tests

--- src/ExamplePkg.jl
+++ src/ExamplePkg.jl
@@ -1,5 +1,14 @@
 module ExamplePkg
 
-greet() = print("Hello World!")
+using Distributions
+
+export pdf_normal
+
+function pdf_normal(x::Number, mu::Number, sigma::Number)
+  dist = Normal(mu,sigma)
+  pdf(dist,x)
+end
 
 end # module
+
+

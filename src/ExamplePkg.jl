f020667ba8d403a95e7eabd674d2feaa95acb988
Eric Ford <ebf11@psu.edu>
2019-04-04 10:07:37 -0400

    docstrings

--- src/ExamplePkg.jl
+++ src/ExamplePkg.jl
@@ -1,9 +1,15 @@
+"Package demontrating how to make your code into a Julia pacakge"
 module ExamplePkg
 
 using Distributions
 
 export pdf_normal
 
+"""
+   pdf_normal(x, mu, sigma)
+
+Return pdf of a normal distribution with mean mu and standard deviation sigma at point x.
+"""
 function pdf_normal(x::Number, mu::Number, sigma::Number)
   dist = Normal(mu,sigma)
   pdf(dist,x)
